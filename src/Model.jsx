90db5a71d2c5772ac3bac597e98a7e2d9bb8282e
Christian <mail@christianhohenbild.de>
2024-03-19 01:16:59 +0100

    adjust postition

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,11 +1,17 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF('/ob1.glb')
+export default function Model(props) {
+  const { nodes, materials } = useGLTF('./models/ob1.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh castShadow receiveShadow geometry={nodes.Cube.geometry} material={materials.Material} />
+      <mesh 
+      castShadow 
+      receiveShadow 
+      geometry={nodes.Cube.geometry} 
+      material={materials.Material} 
+      position={[0, -18, 0]}
+      />
     </group>
   )
 }
