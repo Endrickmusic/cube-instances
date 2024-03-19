83fccc80f872714bf1c0b2f62643b0a3049ddafc
Christian <mail@christianhohenbild.de>
2024-03-20 00:18:54 +0100

    switch to perspective

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ return (
   <>
 
     <Canvas
-    orthographic camera={{ 
-      near: - 1000,
-      far: 1000,
-      zoom: 60, 
-      position: [350, 280, 350]}}
+    // orthographic 
+    camera={{ 
+    //   near: - 1000,
+    //   far: 1000,
+    //   zoom: 60, 
+    //   position: [350, 280, 350]}}
+      position: [3, 2, 3]}}
     >
           <OrbitControls />
     <directionalLight />
